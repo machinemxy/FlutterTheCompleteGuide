c1173e6b23a1c92159a8c735c2910577b5fd10af
ts-xueyuan.ma <ts-xueyuan.ma@rakuten.com>
2021-10-12 16:20:54 +0900

    make quiz and result as separated widget

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,9 +23,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    var questions = const [
+  final _questions = const [
       {
         'questionText': 'What\'s your favorite color?',
         'answers': ['Black', 'Red', 'Green', 'White'],
@@ -40,17 +38,12 @@ class _MyAppState extends State<MyApp> {
       },
     ];
 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('My First App'),),
-        body: Column(
-          children: [
-            Question(questionText: questions[_questionIndex]['questionText'] as String,),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-              return Answer(answerText: answer, selectHandler: _answerQuestion,);
-            })
-          ],
-        ),
+        body: _questionIndex < _questions.length ? Quiz(questions: _questions, questionIndex: _questionIndex, answerQuestion: _answerQuestion,) : const Result(),
       )
     );
   }
